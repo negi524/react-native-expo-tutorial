ce84c01743cfd19f04d1462180cb95ebefe3b612
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-11-02 16:46:08 +0900

    fix: fix config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
+const { defineConfig, globalIgnores } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
+  globalIgnores(["dist/*"]),
   expoConfig,
-  {
-    ignores: ["dist/*"],
-  }
 ]);
